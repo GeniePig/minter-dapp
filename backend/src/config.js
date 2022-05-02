6d1fa1bcf1b7d83607006c18a5506aa5fc57be81
GeniePig <cobianenterprisestx@gmail.com>
2022-05-02 14:49:45 -0500

    took out whitelist address

--- backend/src/config.js
+++ backend/src/config.js
@@ -67,7 +67,7 @@ const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the ro
 const ROYALTY_ADDRESS = "0xE28f52B0F1425390229122a6cBa16DC6673CFB5c"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = ["0xE28f52B0F1425390229122a6cBa16DC6673CFB5c"]; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "Your Contract Address"; // If you want to manually include it
